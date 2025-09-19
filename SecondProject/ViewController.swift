304a000bd0fd0e674f4c911f0baaca0d34210cd4
adigozelovmurad <adigozelov.murad2000@gmail.com>
2025-09-19 15:35:32 +0400

    Initial commit

---
+++ SecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondProject
+//
+//  Created by MURAD on 19.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SecondProjectTests/SecondProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecondProjectTests.swift
+//  SecondProjectTests
+//
+//  Created by MURAD on 19.09.2025.
+//
+
+import Testing
+@testable import SecondProject
+
+struct SecondProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
